21f71c11f935088247ba9e2c03c36e7de2963752
LCPhqt <ple105999@gmail.com>
2025-05-12 16:49:14 +0700

    11

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Dụ mẹ CI/CID</h1>
-      <p>Welcome to my React app with automated deployment 🚀</p>
     </div>
-  );
+
 }
 
 export default App;
